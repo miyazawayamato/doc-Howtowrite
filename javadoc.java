bee2e0926b701013136b06d9db6182227bf55708
miyazawayamato <yamato8781@gmail.com>
2021-07-13 18:05:10 +0900

    7/13

--- javadoc.java
+++ javadoc.java
@@ -4,10 +4,23 @@ class javadoc {
     /**
      * doubleFuncメソッド
      * 引数を2倍にして返す
-     * @param　number 整数値
-     * @return　引数を2倍した整数値
+     * @param number 整数値
+     * @return 引数を2倍した整数値
      */
     public int calcNum(int number) {
         return number * 2;
+    }
+    
+    /**
+     * 
+     * @param firstName
+     * @return String 名前を追加
+     */
+    
+    public String chageName(String firstName) {
+        
+        String name = firstName + "名前";
+        
+        return name;
     }
 }
